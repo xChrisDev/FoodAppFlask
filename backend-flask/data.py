83b2d66aadea4c440f822eab7df72ca88e4f9bd3
xChrisDev <chrissarmiento200203@gmail.com>
2025-03-08 02:36:30 -0600

    Init Project

---
+++ backend-flask/data.py
@@ -0,0 +1,166 @@
+users = [
+    {
+        "id_user": 1,
+        "name": "Kane",
+        "surname": "Johnson",
+        "type": "head_chef",
+        "profile_photo": "https://randomuser.me/api/portraits/men/43.jpg",
+    },
+    {
+        "id_user": 2,
+        "name": "Arthur",
+        "surname": "Hendricks",
+        "type": "sous_chef",
+        "profile_photo": "https://randomuser.me/api/portraits/men/85.jpg",
+    },
+    {
+        "id_user": 3,
+        "name": "Sam",
+        "surname": "Altman",
+        "type": "pastry_chef",
+        "profile_photo": "https://randomuser.me/api/portraits/men/76.jpg",
+    },
+    {
+        "id_user": 4,
+        "name": "Jeremy",
+        "surname": "Stuart",
+        "type": "line_cook",
+        "profile_photo": "https://randomuser.me/api/portraits/men/51.jpg",
+    },
+    {
+        "id_user": 5,
+        "name": "Jared",
+        "surname": "Thompson",
+        "type": "commis_chef",
+        "profile_photo": "https://randomuser.me/api/portraits/men/78.jpg",
+    },
+]
+
+recipes = [
+    {
+        "id_recipe": 1,
+        "id_user": 1,
+        "title": "Classic Beef Wellington",
+        "steps": "Prepare beef wellington",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Beef_Wellington_2019.jpg/640px-Beef_Wellington_2019.jpg",
+    },
+    {
+        "id_recipe": 2,
+        "id_user": 1,
+        "title": "Signature Homemade Sauce",
+        "steps": "Create signature sauce",
+        "image": "https://www.budgetbytes.com/wp-content/uploads/2017/06/Thick-Rich-Pizza-Sauce-finished-1200-500x500.jpg",
+    },
+    {
+        "id_recipe": 3,
+        "id_user": 2,
+        "title": "Creamy Italian Risotto",
+        "steps": "Cook risotto",
+        "image": "https://hips.hearstapps.com/hmg-prod/images/del089923-risotto-web-001-ab-index-64e7a1083b54e.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
+    },
+    {
+        "id_recipe": 4,
+        "id_user": 2,
+        "title": "Grilled Salmon Perfection",
+        "steps": "Grill salmon fillet",
+        "image": "https://mowi-salmon.es/wp-content/uploads/sites/4/2022/05/natural-line-C.jpg",
+    },
+    {
+        "id_recipe": 5,
+        "id_user": 2,
+        "title": "Authentic Seafood Paella",
+        "steps": "Prepare seafood paella",
+        "image": "https://assets.surlatable.com/m/3d3d33b2b4ace4a/72_dpi_webp-slt_REC-266406_1606888_15U_0914.jpg",
+    },
+    {
+        "id_recipe": 6,
+        "id_user": 3,
+        "title": "Decadent Chocolate Cake",
+        "steps": "Bake chocolate cake",
+        "image": "https://grandcafecapecod.com/wp-content/uploads/2024/01/best-chocolate-cake-recipe-from-scratch-8-500x500-1.jpg",
+    },
+    {
+        "id_recipe": 7,
+        "id_user": 3,
+        "title": "Flaky French Croissants",
+        "steps": "Prepare croissants",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/2018_01_Croissant_IMG_0685.JPG/640px-2018_01_Croissant_IMG_0685.JPG",
+    },
+    {
+        "id_recipe": 8,
+        "id_user": 3,
+        "title": "Classic Vanilla Soufflé",
+        "steps": "Make vanilla soufflé",
+        "image": "https://www.allysongofton.co.nz/content/recipes/1959.jpg?width=320&height=479&fit=bounds",
+    },
+    {
+        "id_recipe": 9,
+        "id_user": 4,
+        "title": "Sautéed Mixed Vegetables",
+        "steps": "Sauté vegetables",
+        "image": "https://www.recipevibes.com/wp-content/uploads/2020/04/sauteed-vegetables.jpeg",
+    },
+    {
+        "id_recipe": 10,
+        "id_user": 4,
+        "title": "Juicy Grilled Chicken Breast",
+        "steps": "Grill chicken breast",
+        "image": "https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Garlic_chicken-3f62fd9.jpg?quality=90&resize=556,505",
+    },
+    {
+        "id_recipe": 11,
+        "id_user": 4,
+        "title": "Creamy Mashed Potatoes",
+        "steps": "Prepare mashed potatoes",
+        "image": "https://www.spendwithpennies.com/wp-content/uploads/2021/09/The-Best-Mashed-Potatoes-SpendWithPennies-6.jpg",
+    },
+    {
+        "id_recipe": 12,
+        "id_user": 4,
+        "title": "Traditional French Ratatouille",
+        "steps": "Cook ratatouille",
+        "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjzKtZ8V2SOlAKIKUbOpXVa_8fPdgMGrMXJViWZxKraj-rte2Rb8L2uUSrnQqJE3pn89aay4Z6bwghIPqpMc7Jkla76sLVexbel8Rdwd7oEpaJ0GLSg41XM42-tohf9uqZLUgWkCvOVkw/s1600/000767db.JPG",
+    },
+    {
+        "id_recipe": 13,
+        "id_user": 4,
+        "title": "Homemade Caesar Dressing",
+        "steps": "Make Caesar salad dressing",
+        "image": "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/perfect-caesar-salad-b18f1cb.jpg",
+    },
+    {
+        "id_recipe": 14,
+        "id_user": 5,
+        "title": "Basic Tomato Sauce",
+        "steps": "Make basic tomato sauce",
+        "image": "https://www.budgetbytes.com/wp-content/uploads/2017/06/Thick-Rich-Pizza-Sauce-finished-1200-500x500.jpg",
+    },
+    {
+        "id_recipe": 15,
+        "id_user": 5,
+        "title": "Fluffy Scrambled Eggs",
+        "steps": "Cook scrambled eggs",
+        "image": "https://www.southernliving.com/thmb/9BsC4qqnQEuKi8iqKTf7w85Xwgs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1367022810-29ae41d562df4161bc2039b8bf2d2eff.jpg",
+    },
+    {
+        "id_recipe": 16,
+        "id_user": 5,
+        "title": "Perfect Al Dente Pasta",
+        "steps": "Boil pasta al dente",
+        "image": "https://escueladegastronomíaae.com/wp-content/uploads/2024/09/3-27.png",
+    },
+    {
+        "id_recipe": 17,
+        "id_user": 5,
+        "title": "Simple Homemade Vinaigrette",
+        "steps": "Make simple vinaigrette",
+        "image": "https://images.squarespace-cdn.com/content/v1/64eff5bff72bae08d5a67103/70dbe399-fe29-4786-9154-b9a634ee445b/_MG_4681.jpg",
+    },
+]
+
+
+def get_last_user_id():
+    return users[-1]["id_user"] + 1
+
+def get_last_recipe_id():
+    return recipes[-1]["id_recipe"] + 1
---
+++ backend-flask/web-service.py
@@ -0,0 +1,125 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from data import users, recipes, get_last_recipe_id, get_last_user_id
+
+app = Flask(__name__)
+CORS(app)
+
+
+# ENDPOINTS - USERS
+@app.get("/api/users")
+def get_all_users():
+    return jsonify(users)
+
+
+@app.get("/api/users/<int:id_user>")
+def get_single_user(id_user: int):
+    user_found = [user for user in users if user["id_user"] == id_user]
+    if user_found:
+        return jsonify(user_found[0]), 200
+    return jsonify({"error": "User not found!"}), 404
+
+
+@app.post("/api/users")
+def post_user():
+    if request.is_json:
+        response = request.get_json()
+        if (
+            "name" not in response
+            or "surname" not in response
+            or "type" not in response
+            or "profile_photo" not in response
+        ):
+            return jsonify({"error": "Missing fields!"}), 400
+        response["id_user"] = get_last_user_id()
+        users.append(response)
+        return jsonify({"message": "User created successfully", "user": response}), 201
+    return jsonify({"error": "Request must be JSON"}), 400
+
+
+@app.put("/api/users/<int:id_user>")
+def put_user(id_user: int):
+    if request.is_json:
+        user_found = [user for user in users if user["id_user"] == id_user]
+        if user_found:
+            user_found[0]["name"] = request.json.get("name") or user_found[0]["name"]
+            user_found[0]["surname"] = request.json.get("surname") or user_found[0]["surname"]
+            user_found[0]["type"] = request.json.get("type") or user_found[0]["type"]
+            user_found[0]["profile_photo"] = request.json.get("profile_photo") or user_found[0]["profile_photo"]
+            return jsonify({"message": "User updated successfully", "user": user_found[0]}), 200
+        else:
+            return jsonify({"error": "User not found!"}), 404
+    return jsonify({"error": "Request must be JSON"}), 400
+
+
+@app.delete("/api/users/<int:id_user>")
+def delete_user(id_user: int):
+    user_found = [user for user in users if user["id_user"] == id_user]
+    if user_found:
+        users.remove(user_found[0])
+        return jsonify({"message": "User deleted successfully", "user": user_found[0]}), 200
+    return jsonify({"error": "User not found!"}), 404
+
+
+# ENDPOINTS - RECIPES
+@app.get("/api/users/<int:id_user>/recipes")
+def get_all_recipes_by_user(id_user: int):
+    recipes_found = [recipe for recipe in recipes if recipe["id_user"] == id_user]
+    if recipes_found:
+        return jsonify(recipes_found), 200
+    return jsonify({"error": "This user doesnt have recipes!"}), 404
+
+
+@app.get("/api/users/<int:id_user>/recipes/<int:id_recipe>")
+def get_single_recipe_by_user(id_user: int, id_recipe: int):
+    recipe_found = [recipe for recipe in recipes if recipe["id_user"] == id_user and recipe["id_recipe"] == id_recipe]
+    if recipe_found:
+        return jsonify(recipe_found[0]), 200
+    return jsonify({"error": "Recipe not found!"}), 404
+
+
+@app.post("/api/users/<int:id_user>/recipes")
+def post_recipe(id_user: int):
+    if request.is_json:
+        response = request.get_json()
+        if "name" not in response or "ingredients" not in response or "instructions" not in response:
+            return jsonify({"error": "Missing fields!"}), 400
+        response["id_recipe"] = get_last_recipe_id()
+        response["id_user"] = id_user
+        recipes.append(response)
+        return jsonify({"message": "Recipe created successfully", "recipe": response}), 201
+    return jsonify({"error": "Request must be JSON"}), 400
+
+
+
+@app.put("/api/users/<int:id_user>/recipes/<int:id_recipe>")
+def put_recipe(id_user: int, id_recipe: int):
+    if request.is_json:
+        recipe_found = [recipe for recipe in recipes if recipe["id_user"] == id_user and recipe["id_recipe"] == id_recipe]
+        if recipe_found:
+            recipe = recipe_found[0]
+            recipe["name"] = request.json.get("name") or recipe["name"]
+            recipe["ingredients"] = request.json.get("ingredients") or recipe["ingredients"]
+            recipe["instructions"] = request.json.get("instructions") or recipe["instructions"]
+            return jsonify({"message": "Recipe updated successfully", "recipe": recipe}), 200
+        else:
+            return jsonify({"error": "Recipe not found!"}), 404
+    return jsonify({"error": "Request must be JSON"}), 400
+
+
+
+@app.delete("/api/users/<int:id_user>/recipes/<int:id_recipe>")
+def delete_recipe(id_user: int, id_recipe: int):
+    recipe_found = [
+        recipe
+        for recipe in recipes
+        if recipe["id_user"] == id_user and recipe["id_recipe"] == id_recipe
+    ]
+    if recipe_found:
+        recipes.remove(recipe_found[0])
+        return jsonify({"message": "Recipe deleted successfully", "recipe": recipe_found[0]}), 200
+    return jsonify({"error": "Recipe not found!"}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
